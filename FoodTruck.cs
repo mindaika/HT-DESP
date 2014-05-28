10b091ed853b4059609c8c91b12f89917c4dd967
Randall Sewell <randallsewell@gmail.com>
2014-05-28 12:51:33 -0700

    FoodTruck added

---
+++ FoodTruck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class FoodTruck
+{
+    private List<MenuItem> _menu;
+    private int _truckID;
+    private Status _status;
+    private CustomerInfo _customerInfo;
+
+    public FoodTruck(int truckID)
+	{
+        _truckID = truckID;
+	}
+
+    public void notify(Status status)
+    {
+        _status = status;
+    }
+
+    public List<MenuItem> menuItemList()
+    {
+        return _menu;
+    }
+}
